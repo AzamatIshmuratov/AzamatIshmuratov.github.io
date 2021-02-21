3e7f3b83f9b9fd684ab92b4855179f9dfb458018
Азамат Ишмуратов <36133714+AzamatIshmuratov@users.noreply.github.com>
2021-02-21 14:57:56 +0500

    Create game seaBatle

---
+++ sea_battle_game/src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './Game.css';
+import Game from './Game.js'
+
+
+function App() {
+  return (
+    <Game/>
+  );
+}
+
+export default App;
---
+++ sea_battle_game/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className = "nameGame">Морской бой (одиночные корабли)</div>    
+    <App />
+    <button id = "reload" onClick = {() => {document.location.reload()}}>Начать заново</button>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
